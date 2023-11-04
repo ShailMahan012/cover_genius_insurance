9c22a928f29658ee6cf4281d398bf3f02b7405cb
ShailMurtaza <shailmurtaza@gmail.com>
2023-11-04 12:52:08 +0500

    Created database and signup feature

--- web.py
+++ web.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import logging
 
 app = Flask(__name__)
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+app.app_context().push()
+
+
+class Users(db.Model):
+    __tablename__ = "Users"
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
+    email = db.Column(db.Text)
+    phone = db.Column(db.Text)
 
 
 @app.route("/")
@@ -12,9 +26,18 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+
+        user = Users(first_name=first_name, last_name=last_name, email=email, phone=phone)
+        db.session.add(user)
+        db.session.commit()
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
