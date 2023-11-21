416e7dedd75170f17b5e9fca67be8197472a9c31
ShailMurtaza <shailmurtaza@gmail.com>
2023-11-21 11:00:23 +0500

    aboutus and review updated

---
+++ static/about.js
@@ -0,0 +1,10 @@
+const about = document.getElementById("about")
+
+function show_about() {
+    about.style.left = "0"
+    nav_close()
+}
+
+function hide_about() {
+    about.style.left = "-100%"
+}
